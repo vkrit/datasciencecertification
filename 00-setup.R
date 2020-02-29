9faf8fd06ce5b6fdc4ece4c8713cf5c384f59424
vkrit <veerasak.kritsanapraphan@gmail.com>
2020-02-29 10:34:37 +0700

    add datagenerator.R

--- 00-setup.R
+++ 00-setup.R
@@ -1,6 +1,8 @@
 library(tensorflow)
 library(keras)
 
-install_tensorflow(method = "conda", version = "1.15-gpu", 
+install_tensorflow(method = "conda", version = "1.13.2", 
                    envname = "r-deeplearning")
 
+install_tensorflow(method = "conda", version = "1.13.2-gpu", 
+                   envname = "r-deeplearning")
